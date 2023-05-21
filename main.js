6fe943407b229c9fcfceca8ee0942d06b01f8426
filipzlatojevic <ficozlatojevic@gmail.com>
2023-05-21 10:58:04 +0200

    feat, fix(hero, animation, nav): add hero section with animation and fix overflow on nav.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// import '/qr-code-purchase.svg';
-// import hero from '/home/qr-code-purchase.svg';
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-wrapper nav');
 
@@ -7,8 +5,3 @@ burger.addEventListener('click', () => {
   nav.classList.toggle('active');
   burger.classList.toggle('active');
 });
-
-// console.dir(hero);
-
-// const heroImg = document.querySelector('#hero-img');
-// heroImg.src = hero;
